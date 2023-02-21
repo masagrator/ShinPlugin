f83b63ef95d66e806f8280818c08891d0f0b73e2
MasaGratoR <masagrator@gmail.com>
2023-02-21 21:52:43 +0100

    Add files via upload

--- source/Tfoaf/TextView_Render.hpp
+++ source/Tfoaf/TextView_Render.hpp
@@ -3,17 +3,33 @@
 
 namespace SH1 {
     std::array NMSTextOffsets = {
-        0x4E6A4, //Animated Text
-        0x4EFAC, //Static Text
         0x37C80, //Select
         0x387A0, //After Select
-        0x46F94  //FOAF popup
+        0x46F94, //FOAF popup
+        0x4E6A4, //Animated Text
+        0x4EFAC  //Static Text
     };
 
     // Those offsets are used for Ruby to calculate middle of text
     std::array GetLastXOffsets = {
         0x46128, // Keyword Start
         0x46298 // Keyword End
+    };
+}
+
+namespace SH2 {
+    std::array NMSTextOffsets = {
+        0x3CFBC, //Select
+        0x3DBB0, //After Select
+        0x4E494, //FOAF popup
+        0x53184, //Animated Text
+        0x53A8C  //Static Text
+    };
+
+    // Those offsets are used for Ruby to calculate middle of text
+    std::array GetLastXOffsets = {
+        0x4BD18, // Keyword Start
+        0x4BE88 // Keyword End
     };
 }
 
